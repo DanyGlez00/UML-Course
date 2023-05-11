9ebe547295c9c7134bd7b8124f74afecb641224b
DanyGlez94 <daniel_glez94@hotmail.com>
2023-05-10 23:40:33 -0600

    Accessibility aria, tabs, roles, etc added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const radioButtons = document.querySelectorAll('input[type="radio"]');
+const radioButtons = document.querySelectorAll('.form-input-radio-buttons label');
+const checkButtons = document.querySelectorAll('.form-checkbox label');
 
-  radioButtons.forEach((button) => {
-    button.addEventListener('keydown', (event) => {
+  radioButtons.forEach((label) => {
+    label.addEventListener('keydown', (event) => {
       if (event.code === 'Enter' || event.code === 'Space') {
         event.preventDefault();
-        button.checked = true;
+        const input = label.querySelector('input');
+        input.checked = true;
+      }
+    });
+  });
+
+  checkButtons.forEach((label) => {
+    label.addEventListener('keydown', (event) => {
+      if (event.code === 'Enter' || event.code === 'Space') {
+        event.preventDefault();
+        const input = label.querySelector('input');
+        input.checked = !input.checked;
       }
     });
   });
